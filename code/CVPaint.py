7e91046029ecc22af1b064c61ff0ea8f3c1b69c6
Rachel Won <rachel.won@students.olin.edu>
2018-05-05 20:36:31 -0400

    restructured code

--- code/CVPaint.py
+++ code/CVPaint.py
@@ -39,22 +39,33 @@ def pickColor(point):
         return (0,0,0)
     if 150 < x <= 225 and 0 < y <= 45:
         #purple
-        return (255,0,242)
+        return (242,0,255)
     if 150 < x <= 300 and 0 < y <= 45:
         #blue
-        return (255,0,0)
+        return (0,0,255)
     if 300 < x <= 375 and 0 < y <= 45:
         #green
-        return (0,255,63)
+        return (63,255,0)
     if 375 < x <= 450 and 0 < y <= 45:
         #yellow
-        return (0,250,255)
+        return (255,250,0)
     if 450 < x <= 525 and 0 < y <= 45:
         #orange
-        return (0,174,255)
+        return (255,174,0)
     if 525 < x <= 600 and 0 < y <= 45:
         #red
-        return (0,0,255)
+        return (255,0,0)
+
+def draw_color_bar(frame):
+    # initialize the colorbar (in RGB)
+    cv2.rectangle(frame, (0,0), (75,49), (0,0,0), 2)           # white/eraser
+    cv2.rectangle(frame, (75,0), (150,50), (0,0,0), -1)        # black
+    cv2.rectangle(frame, (150,0), (225,50), (242,0,255), -1)   # violet/purple
+    cv2.rectangle(frame, (225,0), (300,50), (0,0,255), -1)     # blue
+    cv2.rectangle(frame, (300,0), (375,50), (63,255,0), -1)    # green
+    cv2.rectangle(frame, (375,0), (450,50), (255,250,0), -1)   # yellow
+    cv2.rectangle(frame, (450,0), (525,50), (255,174,0), -1)   # orange
+    cv2.rectangle(frame, (525,0), (600,50), (255,0,0), -1)     # red
 
 def save_file(camera, frame):
     #user can input the name of the file when saving
@@ -88,9 +99,11 @@ if __name__ == '__main__':
             pts = []
             linecolor = (0,0,0)
             flag = True
+            counter = 0
             while flag:
                 grabbed, frame = camera.read()
                 frame1 = imutils.resize(frame, width=600)
+                
                 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
                 mask = cv2.inRange(hsv, greenLower, greenUpper)
                 mask = cv2.erode(mask, None, iterations=2)
@@ -101,7 +114,7 @@ if __name__ == '__main__':
                 cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                     cv2.CHAIN_APPROX_SIMPLE)[-2]
                 center = None
-
+                cv2.rectangle(frame1, (0,0), (600,450), (255,255,255), -1)
                 # only proceed if at least one contour was found
                 if len(cnts) > 0:
                     # find the largest contour in the mask, then use
@@ -116,25 +129,13 @@ if __name__ == '__main__':
                     if radius > 10:
                         # draw the circle and centroid on the frame,
                         # then update the list of tracked points
-                        cv2.circle(frame1, (int(x), int(y)), int(radius),
-                             linecolor, 2)
+                        #cv2.circle(frame1, (int(x), int(y)), int(radius), linecolor, 2)
                         cv2.circle(frame1, center, 5, linecolor, -1)
 
                 # update the points queue
-                #pts.appendleft(center)
-                pts.insert(0,(center, linecolor))
+                if counter%2 == 0:
+                    pts.insert(0,(center, linecolor))
                 # loop over the set of tracked points
-                cv2.rectangle(frame1, (0,0), (600,450), (255,255,255), -1)
-
-                # initialize the colorbar (REMEMBER BGR NOT RGB)
-                cv2.rectangle(frame1, (0,0), (75,49), (0,0,0), 2)           # white/eraser
-                cv2.rectangle(frame1, (75,0), (150,50), (0,0,0), -1)        # black
-                cv2.rectangle(frame1, (150,0), (225,50), (255,0,242), -1)   # violet/purple
-                cv2.rectangle(frame1, (225,0), (300,50), (255,0,0), -1)     # blue
-                cv2.rectangle(frame1, (300,0), (375,50), (0,255,63), -1)    # green
-                cv2.rectangle(frame1, (375,0), (450,50), (0,250,255), -1)   # yellow
-                cv2.rectangle(frame1, (450,0), (525,50), (0,174,255), -1)   # orange
-                cv2.rectangle(frame1, (525,0), (600,50), (0,0,255), -1)     # red
 
                 if pts[0][0] is not None:
                     if 0 < pts[0][0][1] <= 45:
@@ -151,15 +152,21 @@ if __name__ == '__main__':
                     #thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
                     cv2.line(frame1, pts[i - 1][0], pts[i][0], pts[i][1], 3)
 
+                draw_color_bar(frame1)
+
                 frame = np.rot90(frame1)
                 frame = pygame.surfarray.make_surface(frame)
                 screen.blit(frame, (0,0))
                 pygame.display.update()
+
                 for event in pygame.event.get():
                     if event.type == KEYDOWN:
                         if event.key == K_q:
                             flag = False
                             save_file(camera, frame1)
+                        if event.key == K_SPACE:
+                            counter += 1
+
         except KeyboardInterrupt:
             pygame.quit()
             cv2.destroyAllWindows()
